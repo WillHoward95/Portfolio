fc456c24a3b4bf9420324dc494c02072153957b6
WillHoward95 <82164172+WillHoward95@users.noreply.github.com>
2021-07-08 15:40:16 +0100

    completed first iteration

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+//phone screen nav menu
 const toggleButton = document.getElementById('toggle-button')
 const navBarLinks = document.getElementById('nav-links');
 
 toggleButton.addEventListener('click', () => {
     navBarLinks.classList.toggle('active');
-    document.getElementById(nav-container).style.backgroundColor = 'red;'
 })
+
+//back to top button
+const backToTop = document.getElementById('back-to-top')
+
+window.addEventListener('scroll', scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 90) {
+        if (!backToTop.classList.contains('btnEntrance')) {
+            backToTop.classList.remove('btnExit')
+            backToTop.classList.add('btnEntrance')
+            backToTop.style.display = 'block'
+        }
+    } else {
+        if (backToTop.classList.contains('btnEntrance')) {
+            backToTop.classList.remove('btnEntrance')
+            backToTop.classList.add('btnExit')
+            
+            setTimeout(function() {
+                backToTop.style.display = 'none'
+            }, 250)
+        }
+
+    }
+}
+
+backToTop.addEventListener('click', backToTopFunction)
+
+function backToTopFunction() {
+    window.scrollTo(0,0);
+}
